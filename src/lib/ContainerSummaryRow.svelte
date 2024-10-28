8bc74f5e3f97ab47973ba69bc15e1a92e5653526
Parker Hill <parkerdh03@gmail.com>
2024-10-27 18:04:21 -0700

    Hopefully final update

--- src/lib/ContainerSummaryRow.svelte
+++ src/lib/ContainerSummaryRow.svelte
@@ -4,7 +4,7 @@
 
 <div class="row">
 	<div class="id">{container.containernumber}</div>
-	<div class="shipname">{container.nameOfship}</div>
+	<div class="shipname">{container.nameofship}</div>
 	<div class="date">{container.datecontainershipped}</div>
 	<div class="size">{container.containersize}</div>
 </div>
--- src/routes/fromDBandComponents/+page.server.js
+++ src/routes/fromDBandComponents/+page.server.js
@@ -4,10 +4,10 @@ import sql from '$lib/server/database';
 export async function load() {
     const rows = await sql`
     SELECT
-        containernumber,
-        nameOfship,
-        containersize,
-        datecontainershipped
+        containerNumber,
+        nameOfShip,
+        containerSize,
+        dateContainerShipped
     FROM
         containers`;
 
